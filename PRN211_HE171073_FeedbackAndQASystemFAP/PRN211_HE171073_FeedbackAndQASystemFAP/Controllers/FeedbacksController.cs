8364648b79124d9a4a20e785cd6767b8420ef27a
maiphuonghoang <maiphuonghoangmpk@gmail.com>
2023-07-10 14:47:52 +0700

    instructor [FB]: list feedback of an instructor page

--- PRN211_HE171073_FeedbackAndQASystemFAP/PRN211_HE171073_FeedbackAndQASystemFAP/Controllers/FeedbacksController.cs
+++ PRN211_HE171073_FeedbackAndQASystemFAP/PRN211_HE171073_FeedbackAndQASystemFAP/Controllers/FeedbacksController.cs
@@ -1,11 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.EntityFrameworkCore;
+using PRN211_HE171073_FeedbackAndQASystemFAP.Models;
+using PRN211_HE171073_FeedbackAndQASystemFAP.Services;
+using System.Security.Claims;
+using System.Collections.Generic;
+using System.Linq;
 namespace PRN211_HE171073_FeedbackAndQASystemFAP.Controllers
 {
     public class FeedbacksController : Controller
     {
-        //public IActionResult Show()
-        //{
-        //}
+        private readonly PRN211_FeedbackAndQASystemContext _context;
+
+        public FeedbacksController(PRN211_FeedbackAndQASystemContext context)
+        {
+            _context = context;
+        }
+        public IActionResult LecturerFeedback()
+        {
+            string roll = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.UserData)?.Value;
+            var feedbacks = _context.Feedbacks
+                            .Include(f => f.Group)
+                                .ThenInclude(g => g.Course)
+                            .Where(f => f.Group.InstructorId.Equals(roll))
+                            .OrderByDescending(f => f.FeedbackCloseDay)
+                            .ToList();
+
+            return View(feedbacks);
+        }
+        public IActionResult FeedbackGPA()
+        {
+            string roll = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.UserData)?.Value;
+            var feedbacks = _context.Feedbacks
+                            .Include(f => f.Group)
+                                .ThenInclude(g => g.Course)
+                            .Where(f => f.Group.InstructorId.Equals(roll))
+                            .OrderByDescending(f => f.FeedbackCloseDay)
+                            .ToList();
+
+            return View(feedbacks);
+        }
     }
 }
---
+++ PRN211_HE171073_FeedbackAndQASystemFAP/PRN211_HE171073_FeedbackAndQASystemFAP/Services/FeedbackService.cs
@@ -0,0 +1,31 @@
+﻿using PRN211_HE171073_FeedbackAndQASystemFAP.Models;
+
+namespace PRN211_HE171073_FeedbackAndQASystemFAP.Services
+{
+    public class FeedbackService
+    {
+        private readonly PRN211_FeedbackAndQASystemContext _context;
+
+        public FeedbackService(PRN211_FeedbackAndQASystemContext context)
+        {
+            _context = context;
+        }
+        public string GetStatusForFeedback(int feedbackId)
+        {
+            DateTime today = DateTime.Now;
+            Feedback f = _context.Feedbacks.FirstOrDefault(f => f.FeedbackId == feedbackId);
+            if (f != null && f.FeedbackOpenDay <= today && today <= f.FeedbackCloseDay)
+            {
+                return "Opening";
+            }
+            else if(f != null && today > f.FeedbackCloseDay)
+            {
+                return "closed"; // You might want to return some default value or handle other cases accordingly
+            }
+            else
+            {
+                return "Future";
+            }
+        }
+    }
+}
